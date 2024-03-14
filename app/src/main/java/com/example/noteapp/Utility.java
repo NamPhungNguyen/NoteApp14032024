8ff292f6b81951ce93c76090db0e5ed27d4f6427
NamPhungNguyen <namhien12082003@gmail.com>
2024-03-14 23:16:53 +0700

    update hien listnotes

--- app/src/main/java/com/example/noteapp/Utility.java
+++ app/src/main/java/com/example/noteapp/Utility.java
@@ -3,10 +3,13 @@ package com.example.noteapp;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
 
 public class Utility {
     static void showToast(Context context, String message) {
@@ -16,5 +19,9 @@ public class Utility {
     static CollectionReference getCollectionReferenceForNotes(){
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         return FirebaseFirestore.getInstance().collection("notes").document(currentUser.getUid()).collection("my_notes");
+    }
+
+    static String timestampToString(Timestamp timestamp){
+        return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
     }
 }
